78df566cfab48969b6fe87e05c8d10bf26786ac7
Mustafa Al-Swaisi <mostfaswaisi93@gmail.com>
2021-04-06 20:07:23 +0300

    Events\Login

--- events/src/app/app.module.ts
+++ events/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { EventsComponent } from './events/events.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EventsComponent
+    EventsComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ events/src/app/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+
+export class LoginComponent {
+
+  email: string;
+  password: string;
+  constructor() { }
+
+  login(loginForm): void {
+    console.log(loginForm);
+    // console.log("Email" , this.email);
+    // console.log("Password" , this.password);
+  }
+
+}
