bf2d98bfcc04ed98c508863ca13ea403eb37ee9e
Mustafa Al-Swaisi <mostfaswaisi93@gmail.com>
2021-04-17 13:19:56 +0300

    course-project

--- course-project/src/app/app.component.ts
+++ course-project/src/app/app.component.ts
@@ -7,9 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'course-project';
-  loadedFeature = 'recipe';
-
-  onNavigate(feature: string): any {
-    this.loadedFeature = feature;
-  }
 }
