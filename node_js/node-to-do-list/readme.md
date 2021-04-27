29cbe3006599b76ce5c20f83c91be90d95e61274
Mustafa Al-Swaisi <mostfaswaisi93@gmail.com>
2021-04-27 11:16:41 +0300

    Update

---
+++ node_js/node-to-do-list/readme.md
@@ -0,0 +1,14 @@
+# To-Do List
+
+Content:
+
+- HTML + CSS
+- JavaScript
+
+## Get the Code
+
+```
+$ git clone https://github.com/mostfaswaisi93/to-do-list
+```
+
+[Demo](https://mostfaswaisi93.github.io/to-do-list/)
--- node_js/node-to-do-list/routes/index.js
+++ node_js/node-to-do-list/routes/index.js
@@ -37,18 +37,16 @@ router.get('/tasks', function(req, res, next) {
 
 router.get('/tasks/create', (req, res) => {
     res.render("task/addEdit", {
-        viewTitle: "Insert task"
+        viewTitle: "Insert Task"
     });
 });
 
 router.post('/tasks/create', (req, res) => {
-    console.log('hi');
-    insertRecord(req, res);
-
-    // if (req.body._id == '')
-    //     insertRecord(req, res);
-    // else
-    //     updateRecord(req, res);
+    // insertRecord(req, res);
+    if (req.body._id == '')
+        insertRecord(req, res);
+    else
+        updateRecord(req, res);
 });
 
 function insertRecord(req, res) {
@@ -64,14 +62,40 @@ function insertRecord(req, res) {
             if (err.name == 'ValidationError') {
                 handleValidationError(err, req.body);
                 res.render("task/addEdit", {
-                    viewTitle: "Insert Task",
-                    task: req.body
+                    viewTitle: "Insert task"
                 });
             } else
                 console.log('Error during record insertion : ' + err);
         }
     });
 }
+
+function updateRecord(req, res) {
+    Task.findOneAndUpdate({ _id: req.body._id }, req.body, { new: true }, (err, doc) => {
+        if (!err) { res.redirect('tasks'); } else {
+            if (err.name == 'ValidationError') {
+                handleValidationError(err, req.body);
+                res.render("task/addEdit", {
+                    viewTitle: 'Update Task',
+                    task: req.body
+                });
+            } else
+                console.log('Error during record update : ' + err);
+        }
+    });
+}
+
+router.get('/tasks', (req, res) => {
+    Task.find((err, docs) => {
+        if (!err) {
+            res.render("task/list", {
+                list: docs
+            });
+        } else {
+            console.log('Error in retrieving task list :' + err);
+        }
+    });
+});
 
 function handleValidationError(err, body) {
     for (field in err.errors) {
@@ -94,10 +118,21 @@ function handleValidationError(err, body) {
     }
 }
 
+router.get('/:id', (req, res) => {
+    Task.findById(req.params.id, (err, doc) => {
+        if (!err) {
+            res.render("task/addEdit", {
+                viewTitle: "Update Task",
+                task: doc
+            });
+        }
+    });
+});
+
 router.get('/delete/:id', (req, res) => {
     Task.findByIdAndRemove(req.params.id, (err, doc) => {
         if (!err) {
-            res.redirect('/task/list');
+            res.redirect('/tasks');
         } else { console.log('Error in task delete :' + err); }
     });
 });
