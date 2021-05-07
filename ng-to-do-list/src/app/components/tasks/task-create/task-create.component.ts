fcf932b99c054e3ffea35b7e8847c0349e0bd17e
Mustafa Al-Swaisi <mostfaswaisi93@gmail.com>
2021-05-07 19:27:58 +0300

    To-Do

--- ng-to-do-list/src/app/components/tasks/task-create/task-create.component.ts
+++ ng-to-do-list/src/app/components/tasks/task-create/task-create.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormGroup, NgForm } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Status } from 'src/app/models/status.model';
 import { Task } from 'src/app/models/task.model';
 import { TasksService } from 'src/app/services/tasks.service';
@@ -19,6 +19,8 @@ export class TaskCreateComponent implements OnInit, OnDestroy {
   task: Task;
   isLoading = false;
   form: FormGroup;
+  private mode = 'create';
+  private taskId: string;
 
   stClass: Status[] = [
     { id: 1, name: 'New' },
@@ -40,6 +42,40 @@ export class TaskCreateComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has('taskId')) {
+        this.mode = 'edit';
+        this.taskId = paramMap.get('taskId');
+        this.isLoading = true;
+        this.tasksService.getTask(this.taskId).subscribe(taskData => {
+          this.isLoading = false;
+          // tslint:disable-next-line:max-line-length
+          this.task = { id: taskData._id, name: taskData.name, date: taskData.date, status: taskData.status, description: taskData.description };
+        });
+      } else {
+        this.mode = 'create';
+        this.taskId = null;
+      }
+    });
+  }
+
+  onSaveTask(form: NgForm): any {
+    if (form.invalid) {
+      return;
+    }
+    this.isLoading = true;
+    if (this.mode === 'create') {
+      this.tasksService.addTask(form.value.name, form.value.date, form.value.status, form.value.description);
+    } else {
+      this.tasksService.updateTask(
+        this.taskId,
+        form.value.name,
+        form.value.date,
+        form.value.status,
+        form.value.description
+      );
+    }
+    form.resetForm();
   }
 
   ngOnDestroy(): any {
--- ng-to-do-list/src/app/services/tasks.service.ts
+++ ng-to-do-list/src/app/services/tasks.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { Task } from '../models/task.model';
 import { map } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -11,24 +12,24 @@ export class TasksService {
   private tasks: Task[] = [];
   private tasksUpdated = new Subject<Task[]>();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   getTasks(): any {
     this.http
-      .get<{ message: string; tasks: any }>(
-        'http://localhost:3000/api/tasks'
+      .get<{ message: string; tasks: any }>('http://localhost:3000/api/tasks')
+      .pipe(
+        map(taskData => {
+          return taskData.tasks.map(task => {
+            return {
+              name: task.name,
+              date: task.date,
+              status: task.status,
+              description: task.description,
+              id: task._id
+            };
+          });
+        })
       )
-      .pipe(map((taskData) => {
-        return taskData.tasks.map(task => {
-          return {
-            name: task.name,
-            date: task.date,
-            status: task.status,
-            description: task.description,
-            id: task._id
-          };
-        });
-      }))
       .subscribe(transformedTasks => {
         this.tasks = transformedTasks;
         this.tasksUpdated.next([...this.tasks]);
@@ -39,20 +40,45 @@ export class TasksService {
     return this.tasksUpdated.asObservable();
   }
 
+  getTask(id: string): any {
+    return this.http.get<{ _id: string; title: string; content: string }>(
+      'http://localhost:3000/api/tasks/' + id
+    );
+  }
+
   addTask(name: string, date: Date, status: string, description: string): any {
     const task: Task = { id: null, name, date, status, description };
     this.http
-      .post<{ message: string, taskId: string }>('http://localhost:3000/api/tasks', task)
+      .post<{ message: string; taskId: string }>(
+        'http://localhost:3000/api/tasks',
+        task
+      )
       .subscribe(responseData => {
         const id = responseData.taskId;
         task.id = id;
         this.tasks.push(task);
         this.tasksUpdated.next([...this.tasks]);
+        this.router.navigate(['/']);
+      });
+  }
+
+  updateTask(id: string, name: string, date: Date, status: string, description: string): any {
+    const task: Task = { id, name, date, status, description };
+    this.http
+      .put('http://localhost:3000/api/tasks/' + id, task)
+      .subscribe(response => {
+        const updatedTasks = [...this.tasks];
+        const oldTaskIndex = updatedTasks.findIndex(p => p.id === task.id);
+        updatedTasks[oldTaskIndex] = task;
+        this.tasks = updatedTasks;
+        this.tasksUpdated.next([...this.tasks]);
+        this.router.navigate(['/']);
       });
   }
 
   deleteTask(taskId: string): any {
-    this.http.delete('http://localhost:3000/api/tasks/' + taskId)
+    this.http
+      .delete('http://localhost:3000/api/tasks/' + taskId)
       .subscribe(() => {
         const updatedTasks = this.tasks.filter(task => task.id !== taskId);
         this.tasks = updatedTasks;
