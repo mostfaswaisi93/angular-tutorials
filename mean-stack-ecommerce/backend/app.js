a6678acd805031081bbc0fd2378a501cc25e1173
Mostafa Alswaisi <mostfaswaisi93@gmail.com>
2021-08-21 01:31:42 +0300

    mean-stack-ecommerce

--- mean-stack-ecommerce/backend/app.js
+++ mean-stack-ecommerce/backend/app.js
@@ -9,16 +9,16 @@ const errorHandler = require('./helpers/error-handler');
 
 
 app.use(cors());
-app.options('*', cors())
+app.options('*', cors());
 
-//middleware
+// Middleware
 app.use(express.json());
 app.use(morgan('tiny'));
 app.use(authJwt());
 app.use('/public/uploads', express.static(__dirname + '/public/uploads'));
 app.use(errorHandler);
 
-//Routes
+// Routes
 const categoriesRoutes = require('./routes/categories');
 const productsRoutes = require('./routes/products');
 const usersRoutes = require('./routes/users');
@@ -31,21 +31,20 @@ app.use(`${api}/products`, productsRoutes);
 app.use(`${api}/users`, usersRoutes);
 app.use(`${api}/orders`, ordersRoutes);
 
-//Database
+// Database
 mongoose.connect(process.env.CONNECTION_STRING, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         dbName: 'eshop-database'
     })
     .then(() => {
-        console.log('Database Connection is ready...')
+        console.log('Database Connection is ready...');
     })
     .catch((err) => {
         console.log(err);
     })
 
-//Server
+// Server
 app.listen(3000, () => {
-
     console.log('server is running http://localhost:3000');
-})
+});
