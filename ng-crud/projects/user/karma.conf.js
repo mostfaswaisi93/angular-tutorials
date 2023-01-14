eeecd758997a4125b1b6f6615755850468d1ff37
mostfaswaisi93 <mostfaswaisi93@gmail.com>
2023-01-14 12:15:06 +0200

    ng-crud

--- ng-crud/projects/user/karma.conf.js
+++ ng-crud/projects/user/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ng-crud'),
+      dir: require('path').join(__dirname, '../../coverage/user'),
       subdir: '.',
       reporters: [
         { type: 'html' },
